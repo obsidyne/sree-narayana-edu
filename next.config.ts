6f40577f5078fab51c4b8fd092cf996b1d246b19
ashvin <ashvinpkumar2004@gmail.com>
2025-08-31 22:14:58 +0530

    ss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'thankful-novelty-5d39f22046.media.strapiapp.com',
+        port: '',
+        pathname: '/**',
+      },
+      // You can add other image domains here if needed
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
